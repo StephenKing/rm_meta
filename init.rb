431dbaa0e9d7a5f930926eac47f250cd07aece82
s-andy <s-andy@faf3ff36-31c7-45dd-a20a-1b001db8e88c>
2013-01-06 22:50:54 +0000

    Fixes for Redmine 1.4.x

--- init.rb
+++ init.rb
@@ -1,21 +1,23 @@
 require 'redmine'
-require 'dispatcher'
 
 require_dependency 'meta_hook'
 
-RAILS_DEFAULT_LOGGER.info 'Starting Meta Tags Plugin for Redmine'
+Rails.logger.info 'Starting Meta Tags Plugin for Redmine'
 
-Dispatcher.to_prepare :meta_plugin do
+Rails.configuration.to_prepare do
+    unless String.method_defined?(:html_safe)
+        String.send(:include, MetaStringHTMLSafePatch)
+    end
     unless ActionView::Base.included_modules.include?(MetaHelper)
         ActionView::Base.send(:include, MetaHelper)
     end
 end
 
-Redmine::Plugin.register :meta_plugin do
+Redmine::Plugin.register :meta do
     name 'Meta tags'
     author 'Andriy Lesyuk'
     author_url 'http://www.andriylesyuk.com'
     description 'Adds an ability to specify meta description and keywords.'
     url 'http://projects.andriylesyuk.com/projects/redmine-meta'
-    version '0.1.0'
+    version '0.2.0'
 end
